a1cd80d86177d60f30e2e75858db5d2821abf009
Tralyn <let1@unlv.nevada.edu>
2019-02-26 23:44:41 -0800

    Got TrackView populating and scrolling through arrow prefabs. Created gameplay controller to handle input controls and syncing with trackview

---
+++ RhythmGame/Assets/Scripts/GamePlayController.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RhythmGame
+{
+    public class GamePlayController : MonoBehaviour
+    {
+        #region Variables
+        [Header("Inputs")]
+        [SerializeField] KeyCode _left = KeyCode.LeftArrow;
+        [SerializeField] KeyCode _down = KeyCode.DownArrow;
+        [SerializeField] KeyCode _up = KeyCode.UpArrow;
+        [SerializeField] KeyCode _right = KeyCode.RightArrow;
+
+        [Header("Track")]
+        [SerializeField] private Track _track;
+        /// <summary>
+        /// The current Track.
+        /// </summary>
+        public Track track { get { return _track; } }
+
+        /// <summary>
+        /// number of seconds per beat.
+        /// </summary>
+        public float secondsPerBeat { get; private set; }
+
+        /// <summary>
+        /// number of beats per second.
+        /// </summary>
+        public float beatsPerSecond { get; private set; }
+
+        /// <summary>
+        /// If the player has played within the current beat
+        /// </summary>
+        private bool _played;
+
+        /// <summary>
+        /// Track completed
+        /// </summary>
+        private bool _completed;
+
+        private TrackView _trackView; 
+        private WaitForSeconds waitAndStop;
+        #endregion
+
+        #region Singleton
+        private static GamePlayController _instance;
+        /// <summary>
+        /// Current GamePlayController instance.
+        /// </summary>
+        public static GamePlayController Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = (GamePlayController)GameObject.FindObjectOfType(typeof(GamePlayController));
+
+                return _instance;
+            }
+        }
+
+        void OnDestroy()
+        {
+            _instance = null;
+        }
+        #endregion
+
+        #region MonoBehaviour Methods
+        void Awake()
+        {
+            _instance = this;
+            secondsPerBeat = track.bpm / 60f;
+            beatsPerSecond = 60f / track.bpm;
+            waitAndStop = new WaitForSeconds(beatsPerSecond * 2);
+
+            _trackView = FindObjectOfType<TrackView>();
+            if (!_trackView)
+                Debug.LogWarning("No TrackView found in current scene");
+        }
+
+        void Start()
+        {
+            InvokeRepeating("NextBeat", 0f, beatsPerSecond);
+        }
+
+        void Update()
+        {
+            if (_played || _completed)
+                return;
+
+            if (Input.GetKeyDown(_left))
+                PlayBeat(0);
+            if (Input.GetKeyDown(_down))
+                PlayBeat(1);
+            if (Input.GetKeyDown(_up))
+                PlayBeat(2);
+            if (Input.GetKeyDown(_right))
+                PlayBeat(3);
+        }
+        #endregion
+
+        #region GamePlay
+        private int _current;
+        /// <summary>
+        /// The current beat index.
+        /// </summary>
+        public int current
+        {
+            get { return _current; }
+            private set
+            {
+                if (value != _current)
+                {
+                    _current = value;
+
+                    if (_current == _track.beats.Count)
+                    {
+                        CancelInvoke("NextBeat");
+
+                        _completed = true;
+
+                        StartCoroutine(WaitAndStop());
+                    }
+                }
+            }
+        }
+
+        void PlayBeat(int input)
+        {
+            _played = true;
+
+            if (_track.beats[current] == -1)
+            {
+                Debug.Log (string.Format("{0} played untimely", input));
+            }
+            else if (_track.beats[current] == input)
+            {
+                _trackView.TriggerBeatView(current, TrackView.Trigger.Right);
+            }
+            else
+            {
+                _trackView.TriggerBeatView(current, TrackView.Trigger.Wrong);
+            }
+        }
+
+        void NextBeat()
+        {
+
+            if (!_played && _track.beats[current] != -1)
+            {
+                _trackView.TriggerBeatView(current, TrackView.Trigger.Missed);
+            }
+            _played = false;
+
+            current++;
+        }
+
+        private IEnumerator WaitAndStop()
+        {
+            yield return waitAndStop;
+            enabled = false;
+        }
+        #endregion
+    }
+}
---
+++ RhythmGame/Assets/Scripts/TrackView.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RhythmGame
+{
+    /// <summary>
+    /// Track View Component.
+    /// </summary>
+    [AddComponentMenu("UnityCoach/Beats/TrackView")]
+    [RequireComponent(typeof(VerticalLayoutGroup))]
+    [RequireComponent(typeof(ContentSizeFitter))]
+    [RequireComponent(typeof(RectTransform))]
+    public class TrackView : MonoBehaviour
+    {
+        public enum Trigger
+        {
+            Missed,
+            Right,
+            Wrong
+        }
+
+        //      [SerializeField] Track _track;
+
+        [Header("Beats' Display")]
+        [SerializeField] RectTransform _left;
+        [SerializeField] RectTransform _right;
+        [SerializeField] RectTransform _up;
+        [SerializeField] RectTransform _down;
+        [SerializeField] RectTransform _empty;
+
+        RectTransform _rTransform; 
+        List<Image> _beatViews;
+
+        Vector2 _position; 
+        float _beatViewSize; 
+        float _spacing; 
+
+        /// <summary>
+        /// The scrolling position of the TrackView.
+        /// </summary>
+        public float position
+        {
+            get { return _position.y; }
+            set
+            {
+                if (value != _position.y)
+                {
+                    _position.y = value;
+                    _rTransform.anchoredPosition = _position;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initialises the TrackView with the specified Track.
+        /// </summary>
+        /// <param name="track">The Track to initialise the TrackView with.</param>
+        public void Init(Track track)
+        {
+            _rTransform = (RectTransform)transform;
+            _position = _rTransform.anchoredPosition;
+
+            _beatViewSize = _empty.rect.height;
+            _spacing = GetComponent<VerticalLayoutGroup>().spacing;
+
+            _beatViews = new List<Image>();
+
+            foreach (int b in track.beats)
+            {
+                GameObject g;
+                switch (b)
+                {
+                    case 0:
+                        g = _left.gameObject;
+                        break;
+                    case 1:
+                        g = _down.gameObject;
+                        break;
+                    case 2:
+                        g = _up.gameObject;
+                        break;
+                    case 3:
+                        g = _right.gameObject;
+                        break;
+                    default:
+                        g = _empty.gameObject;
+                        break;
+                }
+                Image view = GameObject.Instantiate(g, transform).GetComponent<Image>();
+                view.transform.SetAsFirstSibling();
+
+                _beatViews.Add(view);
+            }
+        }
+
+        void Start()
+        {
+            Init(GamePlayController.Instance.track);
+        }
+
+        void Update()
+        {
+            position -= (_beatViewSize + _spacing) * Time.deltaTime * GamePlayController.Instance.secondsPerBeat;
+        }
+
+        /// <summary>
+        /// Triggers beat view update.
+        /// </summary>
+        /// <param name="index">Index of the Beat View to update.</param>
+        /// <param name="trigger">Update Trigger Type.</param>
+        public void TriggerBeatView(int index, Trigger trigger)
+        {
+            switch (trigger)
+            {
+                case Trigger.Missed:
+                    _beatViews[index].color = Color.gray;
+                    break;
+                case Trigger.Right:
+                    _beatViews[index].color = Color.yellow;
+                    break;
+                case Trigger.Wrong:
+                    _beatViews[index].color = Color.cyan;
+                    break;
+            }
+        }
+    }
+}
